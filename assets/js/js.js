c1fc1c1c4282b29a40baf1d1b1a5fa096e7aa4ac
ingriarce4 <ingridarce4@gmail.com>
2017-05-22 10:25:58 -0400

    cambios dos

---
+++ assets/js/js.js
@@ -0,0 +1,17 @@
+
+//Instrucciones para borrar cada panda-imagen: impresión de imagen, evento
+document.getElementById("t1").addEventListener("click",function(){
+	document.getElementById("fotot1").classList.toggle("desaparecepanda");
+})
+
+document.getElementById("t2").addEventListener("click",function(){
+	document.getElementById("fotot2").classList.toggle("desaparecepanda");
+})
+
+document.getElementById("t3").addEventListener("click",function(){
+	document.getElementById("fotot3").classList.toggle("desaparecepanda");
+})
+
+document.getElementById("t4").addEventListener("click",function(){
+	document.getElementById("fotot4").classList.toggle("desaparecepanda");
+})
